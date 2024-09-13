55e8e4dc69e7adb22b0131f1d9ab022ae66c0ec4
Thitroy <fernanda.morales2001@alumnos.ubiobio.cl>
2024-09-12 21:08:12 -0300

    eliminacion de datos guía

--- clase2/arreglos.js
+++ clase2/arreglos.js
@@ -1,11 +1,5 @@
 
 //Ejercicio 7
-/**
- * 1. Crea una función que reciba como parámetro un array de números y retorne la suma de todos los números del array.
-2. Crea una función ( o varias) que reciba como parámetro un array de números y retorne el promedio de todos los números del array.
-3. Crea una función que tome un arreglo de strings como parámetro y devuelva un nuevo arreglo que contenga los mismos strings pero con todas las letras en mayúsculas.
-4. Crea una función que tome un arreglo de números como parámetro y devuelva un nuevo arreglo que contenga solo los números pares.
-**/
 function arregloSuma(arreglo){
     let suma = 0;
     for (let i = 0; i < arreglo.length; i++){
